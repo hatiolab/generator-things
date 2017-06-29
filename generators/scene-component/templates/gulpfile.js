f0cd2d736038dba6df9d1d3ef7895e21d0276167
heartyoh <heartyoh@hatiolab.com>
2017-06-29 23:16:05 +0900

    fix gulp file for license notice header

--- generators/scene-component/templates/gulpfile.js
+++ generators/scene-component/templates/gulpfile.js
@@ -5,17 +5,16 @@ var license = require('gulp-header-license');
 var fs = require('fs');
  
 gulp.task('license', function () {
-    var year = (new Date()).getFullYear();
     gulp.src('./src/**/*.js')
-            .pipe(license('/*\n * Copyright © ${year}, HatioLab Inc. All rightss reserved.\n */\n', {year: year}))
+            .pipe(license('/*\n * Copyright © HatioLab Inc. All rights reserved.\n */\n'))
             .pipe(gulp.dest('./src/'));
     gulp.src('./test/**/*.js')
-            .pipe(license('/*\n * Copyright © ${year}, HatioLab Inc. All rightss reserved.\n */\n', {year: year}))
+            .pipe(license('/*\n * Copyright © HatioLab Inc. All rights reserved.\n */\n'))
             .pipe(gulp.dest('./test/'));
     gulp.src('./test/**/*.html')
-            .pipe(license('<!--\n@license\nCopyright © ${year}, HatioLab Inc. All rightss reserved.\n-->', {year: year}))
+            .pipe(license('<!--\n@license\nCopyright © HatioLab Inc. All rights reserved.\n-->'))
             .pipe(gulp.dest('./test/'));
     gulp.src('./demo/**/*.html')
-            .pipe(license('<!--\n@license\nCopyright © ${year}, HatioLab Inc. All rightss reserved.\n-->', {year: year}))
+            .pipe(license('<!--\n@license\nCopyright © HatioLab Inc. All rights reserved.\n-->'))
             .pipe(gulp.dest('./demo/'));
 });
