84281e51f2303cfeaf63ffdbdf1148db73f40a3e
Hearty Oh <heartyoh@hatiolab.com>
2019-02-21 19:44:26 +0900

    v0.4.6
    remove vscode config folder

--- generators/incubator/index.js
+++ generators/incubator/index.js
@@ -2,64 +2,72 @@
  * Copyright © HatioLab Inc. All rights reserved.
  */
 
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const path = require('path');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const path = require("path");
 
 function classname(str) {
-  return str.replace(/-/g, ' ').replace(/(?:^\w|[A-Z]|\b\w)/g, function (letter, index) {
-    return letter.toUpperCase();
-  }).replace(/\s+/g, '');
+  return str
+    .replace(/-/g, " ")
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+      return letter.toUpperCase();
+    })
+    .replace(/\s+/g, "");
 }
 
 module.exports = class extends Generator {
   prompting() {
+    this.log(
+      yosay(
+        "Welcome to the smashing " +
+          chalk.red("generator-things:incubator") +
+          " generator!"
+      )
+    );
 
-    this.log(yosay(
-      'Welcome to the smashing ' + chalk.red('generator-things:incubator') + ' generator!'
-    ));
-
-    const prompts = [{
-      type: 'input',
-      name: 'moduleName',
-      message: 'Your things module name?',
-      default: this.appname.replace(/ /g, '-')
-    }, {
-      type: 'input',
-      name: 'username',
-      message: 'What\'s your Github username',
-      store: true
-    }];
+    const prompts = [
+      {
+        type: "input",
+        name: "moduleName",
+        message: "Your things module name?",
+        default: this.appname.replace(/ /g, "-")
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "What's your Github username",
+        store: true
+      }
+    ];
 
     return this.prompt(prompts).then(props => {
       this.props = props;
 
-      this.props.simpleModuleName = props.moduleName.replace('things-scene-', '');
+      this.props.simpleModuleName = props.moduleName.replace(
+        "things-scene-",
+        ""
+      );
     });
   }
 
   writing() {
     var tpl = this.props;
 
-    this.fs.copyTpl([
-      this.templatePath() + '/**',
-      this.templatePath() + '/**/.*',
-      '!**/{.DS_Store,_*}/**'],
+    this.fs.copyTpl(
+      [
+        this.templatePath() + "/**",
+        this.templatePath() + "/**/.*",
+        "!**/{.DS_Store,_*}/**"
+      ],
       this.destinationPath(),
       tpl
     );
 
     this.fs.copyTpl(
-      this.templatePath('_gitignore'),
-      this.destinationPath('.gitignore'),
-      tpl
-    );
-
-    this.fs.copyTpl(
-      this.templatePath('_vscode'),
-      this.destinationPath('.vscode'),
+      this.templatePath("_gitignore"),
+      this.destinationPath(".gitignore"),
       tpl
     );
   }
